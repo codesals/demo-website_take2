2fcb92f26505b404f3b15e2a7f61932435a61a5d
Sally <78157471+codesals@users.noreply.github.com>
2021-02-14 15:24:40 +0300

    Add content

---
+++ script.js
@@ -0,0 +1,11 @@
+// function reset() {
+//   document.getElementById("kwd_input").value = 0;
+//   document.getElementById("usd_input").value = 0;
+// }
+
+// var myModal = document.getElementById("myModal");
+// var myInput = document.getElementById("myInput");
+
+// myModal.addEventListener("shown.bs.modal", function () {
+//   myInput.focus();
+// });
